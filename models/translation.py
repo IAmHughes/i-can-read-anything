0515e341e7c3fb9132705cb949068f8cef2a8326
Thomas Hughes <2894107+IAmHughes@users.noreply.github.com>
2023-03-21 12:29:31 -0400

    Update translation.py

--- models/translation.py
+++ models/translation.py
@@ -1,1 +1,9 @@
+# models/translation.py
+from app import db
 
+class Translation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text)
+    translation = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
