27ae42e985616e04b7e732f6bc2822913b3cb2bb
Thomas Hughes <2894107+IAmHughes@users.noreply.github.com>
2024-03-11 17:20:55 -0400

    Remove old app, starting over

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-SECRET_KEY = "your_secret_key_here"  # Change this to a strong, unique value
